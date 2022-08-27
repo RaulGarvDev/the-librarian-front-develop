19bbf5b16de80e3e29367877a7a1c05a85d8a8fc
RaulGarvDev <raulgarvdev@gmail.com>
2022-08-27 22:51:51 +0200

    deploy

--- LibrarianFront/src/environments/environment.ts
+++ LibrarianFront/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
 
 
-  production: false,
+  production: true,
   urlBooksRandom:  `${urlBasic}/get`,
   urlAddBook:  `${urlBasic}/createBook`,
   urlAddReserve:  `${urlBasic}/reserve`,
