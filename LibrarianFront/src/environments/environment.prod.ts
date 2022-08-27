8c553e9eaa7d5a248fb55ee83564dc38d8ad12a4
RaulGarvDev <raulgarvdev@gmail.com>
2022-08-27 22:57:01 +0200

    deploy

--- LibrarianFront/src/environments/environment.prod.ts
+++ LibrarianFront/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
+const urlBasic: string = "88.1.205.136:9595";
+
 export const environment = {
-  production: true
+
+
+  production: true,
+  urlBooksRandom:  `${urlBasic}/get`,
+  urlAddBook:  `${urlBasic}/createBook`,
+  urlAddReserve:  `${urlBasic}/reserve`,
+  urlPdf: `${urlBasic}/reserve/reserve/export/pdf`,
+  urlChangePassword:  `${urlBasic}/users/changePassword`,
+  urlReservedByUser: `${urlBasic}/reserve/reservedBooksByUserId`,
+  urlAllReservesByUser: `${urlBasic}/reserve/getAllReservationById`,
+  urlAllBooksByIdBD:  `${urlBasic}/getByIdBook`,
+
 };
