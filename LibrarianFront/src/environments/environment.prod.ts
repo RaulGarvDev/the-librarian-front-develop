f3d4307b580ef99bad1f8108918c7e4f180a0cf6
RaulGarvDev <raulgarvdev@gmail.com>
2022-08-28 12:35:47 +0200

    deploy

--- LibrarianFront/src/environments/environment.prod.ts
+++ LibrarianFront/src/environments/environment.prod.ts
@@ -1,16 +1,16 @@
-//const urlBasic: string = "88.1.205.136:9595";
+const urlBasic: string = "88.1.205.136:9595";
 
 export const environment = {
 
 
   production: true,
-  urlBooksRandom:  `88.1.205.136:9595/get`,
-  urlAddBook:  `88.1.205.136:9595/createBook`,
-  urlAddReserve:  `88.1.205.136:9595/reserve`,
-  urlPdf: `88.1.205.136:9595/reserve/reserve/export/pdf`,
-  urlChangePassword:  `88.1.205.136:9595/users/changePassword`,
-  urlReservedByUser: `88.1.205.136:9595/reserve/reservedBooksByUserId`,
-  urlAllReservesByUser: `88.1.205.136:9595/reserve/getAllReservationById`,
-  urlAllBooksByIdBD:  `88.1.205.136:9595/getByIdBook`,
+  urlBooksRandom:  `${urlBasic}/get`,
+  urlAddBook:  `${urlBasic}/createBook`,
+  urlAddReserve:  `${urlBasic}/reserve`,
+  urlPdf: `${urlBasic}/reserve/reserve/export/pdf`,
+  urlChangePassword:  `${urlBasic}/users/changePassword`,
+  urlReservedByUser: `${urlBasic}/reserve/reservedBooksByUserId`,
+  urlAllReservesByUser: `${urlBasic}/reserve/getAllReservationById`,
+  urlAllBooksByIdBD:  `${urlBasic}/getByIdBook`,
 
 };
