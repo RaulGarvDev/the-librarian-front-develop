51112346eaacd8aa44f1002ef685e4656420ff88
RaulGarvDev <raulgarvdev@gmail.com>
2022-08-28 10:42:14 +0200

    deploy

--- LibrarianFront/src/app/home/services/book.service.ts
+++ LibrarianFront/src/app/home/services/book.service.ts
@@ -24,7 +24,7 @@ export class BookService {
       'Content-Type':  'application/json',
       "Access-Control-Allow-Origin": "*",
       
-    } ),responseType: 'text' as 'json'
+    } ),responseType: 'any' as 'any'
   };
 
 
